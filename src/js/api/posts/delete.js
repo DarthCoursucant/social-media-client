e17c741be837d4cea4c9d51a478e39e6fa183fe2
AKB <Ameliakingabujar2002@gmail.com>
2024-10-21 14:41:07 +0200

    Update delete.js

--- src/js/api/posts/delete.js
+++ src/js/api/posts/delete.js
@@ -1,15 +1,1 @@
-import { apiPath } from "../constants.js";
-import { headers } from "../headers.js";
-
-export async function deletePost(id) {
-	const response = await fetch(`${apiPath}/social/posts/${id}`, {
-		method: "delete",
-		headers: headers(),
-	});
-
-	if (response.ok) {
-		return await response.json();
-	}
-
-	throw new Error(response.statusText);
-}
+export async function deletePost(id) {}
